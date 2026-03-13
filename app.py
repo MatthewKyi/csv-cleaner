a52bd8452d643005facf58a0c7dd08507a28d942
Matthew Kyi <mkyi@students.cs.ubc.ca>
2026-03-13 14:04:15 -0700

    Prep for production

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, send_file
 import pandas as pd
 from io import BytesIO
+import os
 
 app = Flask(__name__)
 
@@ -86,4 +87,4 @@ def process():
 
 
 if __name__ == "__main__":
-    app.run(port=5001)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
